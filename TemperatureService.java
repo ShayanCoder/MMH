8c4a71811bbfd6901d913860e52a25eb1ec191f1
ShayanCoder <39023941+ShayanCoder@users.noreply.github.com>
2023-07-24 17:37:08 +0530

    Add files via upload

---
+++ TemperatureService.java
@@ -0,0 +1,22 @@
+package com.mh.demo.service;
+
+import com.ayo.demo.service.convertor.Convertor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.math3.util.Precision;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class TemperatureService implements Convertor {
+
+
+    @Override
+    public double convertToMetric(final double fahrenheitValue) {
+        return Precision.round((fahrenheitValue - 32) / 1.8, 2);
+    }
+
+    @Override
+    public double convertToImperial(final double celsiusValue) {
+        return Precision.round((celsiusValue * 1.8) + 32, 2);
+    }
+}
